26341a4b576414342ce6dc1b83a9c63abf466249
DuyLong <duylongmind432001@gmail.com>
2026-01-17 19:14:43 +0700

    Add file to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ ENV_FILE="/usr/share/nginx/html/env-config.js"
 echo "window._env_ = {" > $ENV_FILE
 echo "  VITE_CLOUDFLARE_API_BEAR: \"$VITE_CLOUDFLARE_API_BEAR\"," >> $ENV_FILE
 echo "  VITE_TAILSCALE_USERNAME: \"$VITE_TAILSCALE_USERNAME\"" >> $ENV_FILE
+echo "  VITE_TRUENAS_BEAR: \"$VITE_TRUENAS_BEAR\"" >> $ENV_FILE
 echo "};" >> $ENV_FILE
 
 echo "✅ Generated runtime environment config at $ENV_FILE"
